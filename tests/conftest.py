9819867d244ed9dbdd87845d75cc093ff92f75b8
Toni Shamraev <toni-shamraev@yandex.com>
2025-01-19 20:15:12 +0300

    second testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,3 +4,11 @@ import pytest
 @pytest.fixture
 def maestro():
     return "Maestro 1596 83** **** 5199"
+
+@pytest.fixture
+def mastercard():
+    return "MasterCard 7158 30** **** 6758"
+
+@pytest.fixture
+def empty_string():
+    return ""
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -2,3 +2,10 @@ from src.masks import get_mask_card_number
 
 def test_get_mask_card_number(maestro):
     assert get_mask_card_number("Maestro 1596837868705199") == maestro
+
+
+def test_get_mask_card_number(mastercard):
+    assert get_mask_card_number("MasterCard 7158300734726758") == mastercard
+
+def test_get_mask_card_number(empty_string):
+    assert get_mask_card_number("") == empty_string
